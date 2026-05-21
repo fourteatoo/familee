4e20a0c9edfb8e6efc801bc12c0af96b4b53d01d
Walter Pelissero <walter@pelissero.de>
2026-05-21 23:54:56 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.fourteatoo/familee "0.1.0-SNAPSHOT"
+(defproject io.github.fourteatoo/familee "0.1.0"
   :description "Google Family Link tool"
   :url "http://github.com/fourteatoo/familee"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
